0c5f7fa15bae27c4977e6c97afe17caf5386a047
yashwanthkamana <yashwanth123.kamana@gmail.com>
2023-09-17 19:55:16 +0530

    feat: add basic deps

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  RecoilRoot,
+} from 'recoil';
+
+function App() {
+  return (
+    <RecoilRoot>
+      <div className='text-3xl'>hello</div>
+    </RecoilRoot>
+  );
+}
+
+export default App;
